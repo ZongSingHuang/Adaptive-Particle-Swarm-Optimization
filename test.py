9bb87134e92a370a748c3f4ff8c0649dc5aaf287
ZongSingHuang <58209813+ZongSingHuang@users.noreply.github.com>
2020-10-12 15:04:33 +0800

    2020.10.12

--- test.py
+++ test.py
@@ -691,7 +691,7 @@ all_for_loss.columns=['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7',
                       'F8', 'F9', 'F10', 'F11', 'F12', 
                       'F13', 'F14', 'F15', 'F16', 'F17', 'F18',
                       'F19', 'F20', 'F21', 'F22', 'F23']
-ax = all_for_loss.plot(kind='line', grid=True, legend=True, logy=True)
-ax.set_title('WOA')
+ax = all_for_loss.plot(kind='line', grid=True, legend=True)
+ax.set_title('APSO')
 ax.set_xlabel('iteration')
 ax.set_ylabel('fitness value')
